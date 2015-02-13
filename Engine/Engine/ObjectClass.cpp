9b0fdd82cec61913c908a44c684556f94eb9ccdb
Home - Peter <dastykill@gmail.com>
2015-02-14 00:22:08 +0100

    PV

--- Engine/Engine/ObjectClass.cpp
+++ Engine/Engine/ObjectClass.cpp
@@ -138,9 +138,9 @@ void ObjectClass::interpolateFrames(Frame* f1, Frame* f2, XMFLOAT4X4* m)
 {
 	float t1 = f1->time;
 	float t2 = f2->time;
-	
+	float time = mTime->TotalTime() / 1.25;
 
-	float lT = 1;// (mTime->TotalTime() - t1) / (t2 - t1);
+	float lT = (time - t1) / (t2 - t1);
 
 	XMVECTOR lerpedT;
 	XMVECTOR lerpedS;
@@ -177,47 +177,27 @@ void ObjectClass::Animate(XMFLOAT4X4** mBL)
 	UINT count = mModel->GetBoneCount();
 	AnimClipRead* aC = mModel->GetAnimationClips();
 	XMFLOAT4X4 mat;
-	interpolateFrames(&aC[0].bones[0].Frames[40], &aC[0].bones[0].Frames[40], &mat);
-	
-	XMMATRIX w = XMMatrixRotationX(0); //XMMatrixRotationQuaternion(XMVectorSet(mTime->TotalTime() / 10.f, 0, 0, 0)); //XMLoadFloat4x4(&mat);//XMMatrixTranslationFromVector(XMVectorSet(mTime->TotalTime(), 0, 0, 0));
-	XMMATRIX l = XMLoadFloat4x4(&bones[0].localOffset);
-	XMStoreFloat4x4(&bones[0].globalOffset, w*l);
 
+	UINT frame = mTime->TotalTime() / (1.25 * 76);
+
+	// Interpolate first bone
+	interpolateFrames(&aC[0].bones[0].Frames[frame], &aC[0].bones[0].Frames[frame+1], &mat);
+	XMMATRIX w;
+	XMMATRIX g = XMLoadFloat4x4(&mat);//XMMatrixRotationX(0); //XMMatrixRotationQuaternion(XMVectorSet(mTime->TotalTime() / 10.f, 0, 0, 0)); //XMLoadFloat4x4(&mat);//XMMatrixTranslationFromVector(XMVectorSet(mTime->TotalTime(), 0, 0, 0));
+	XMMATRIX l = XMLoadFloat4x4(&bones[0].localOffset);
+	XMStoreFloat4x4(&bones[0].globalOffset, g*l);
+	// Rest of the bones
 	for (UINT i = 1; i < count; i++)
 	{
-		w = XMLoadFloat4x4(&bones[bones[i].ParentBone].globalOffset);
+		interpolateFrames(&aC[0].bones[i].Frames[frame], &aC[0].bones[i].Frames[frame+1], &mat);
+		w = XMLoadFloat4x4(&mat);
+		g = XMLoadFloat4x4(&bones[bones[i].ParentBone].globalOffset);
 		l = XMLoadFloat4x4(&bones[i].localOffset);
-		XMStoreFloat4x4(&bones[i].globalOffset, l*w);
+		XMStoreFloat4x4(&bones[i].globalOffset, l*g*w);
 	}
 
 
-	//AnimClipRead* aC = mModel->GetAnimationClips();
-	//int time = floorf(mTime->TotalTime());
-
-	//if (mTime->TotalTime() >= 1.25f)
-	//{
-	//	mTime->Reset();
-	//	mTime->Start();
-	//	mTime->Tick();
-	//	mDCount = 0;
-	//}
-
-	//XMFLOAT4X4 mat;
-	//interpolateFrames(&aC[0].bones[0].Frames[mDCount], &aC[0].bones[0].Frames[mDCount + 1], &mat);
-	//XMMATRIX w = XMLoadFloat4x4(&mat);
-	//XMMATRIX l = XMLoadFloat4x4(&bones[0].localOffset);
-	//XMStoreFloat4x4(&bones[0].globalOffset, l*w);
-
-	//for (UINT i = 1; i < count; i++)
-	//{
-	//	//interpolateFrames(&aC[0].bones[0].Frames[time], &aC[0].bones[0].Frames[time + 1], &mat);
-	//	w = XMLoadFloat4x4(&bones[bones[i].ParentBone].globalOffset); //w = XMLoadFloat4x4(&mat);
-	//	l = XMLoadFloat4x4(&bones[i].localOffset);
-	//	XMStoreFloat4x4(&bones[i].globalOffset, l*w);
-	//}
-
-
-
+	// Loads the bone matrices into the constant buffer dataPtr
 	for (UINT i = 0; i < count; i++)
 	{
 		XMMATRIX mat = XMLoadFloat4x4(&bones[i].globalOffset);
@@ -225,26 +205,4 @@ void ObjectClass::Animate(XMFLOAT4X4** mBL)
 		XMStoreFloat4x4(&m[i], XMMatrixTranspose(BP*mat));
 	}
 
-
-
-
-	//XMMATRIX w = XMLoadFloat4x4(&mat);
-	//XMMATRIX bP = XMLoadFloat4x4(&bones[0].invBindPose);
-	//XMMATRIX fin;
-	//XMStoreFloat4x4(&bones[0].globalOffset, w);
-	//XMStoreFloat4x4(&m[0], XMMatrixTranspose(bP*w));
-
-	//
-
-	//for (UINT i = 1; i < count; i++)
-	//{
-	//	Bone& b = bones[i];
-	//	interpolateFrames(&aC[0].bones[i].Frames[time], &aC[0].bones[i].Frames[time + 1], &mat);
-	//	w = XMLoadFloat4x4(&mat);
-	//	XMMATRIX bP = XMLoadFloat4x4(&b.invBindPose);
-	//	XMMATRIX g = XMLoadFloat4x4(&bones[b.ParentBone].globalOffset);
-	//	fin = w*g;
-	//	XMStoreFloat4x4(&b.globalOffset, fin);
-	//	XMStoreFloat4x4(&m[i], XMMatrixTranspose(bP*fin));
-	//}
 }
